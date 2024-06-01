d9cab995ddb82783eb601f37088a3aaae51bc025
bit-barron <1bit.baron@gmail.com>
2024-06-01 20:33:50 +0200

    asd

--- api/llm/model.py
+++
@@ -1,6 +0,0 @@
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.document_loaders import TextLoader
-from langchain_community.embeddings.sentence_transformer import (SentenceTransformerEmbeddings)
-from langchain_community.vectorstores import Chroma
-
-loader = TextLoader('state_of_the_union.txt', encoding="utf-8")
