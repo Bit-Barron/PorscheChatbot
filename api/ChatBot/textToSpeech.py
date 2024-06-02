59353700302369e36a2b418782f4cdecee1186c5
bit-barron <1bit.baron@gmail.com>
2024-06-02 14:39:04 +0200

    asd

--- api/ChatBot/textToSpeech.py
+++ api/ChatBot/textToSpeech.py
@@ -1,7 +1,7 @@
 import openai
 import os
 from flask_cors import CORS
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request, jsonify
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -11,23 +11,28 @@ textToSpeech_bp = Blueprint('textToSpeech', __name__)
 
 
 def create_voice(solution):
+    print("Creating Voice", solution)
     response = openai.audio.speech.create(model="tts-1", voice="echo", input=solution)
 
-    with open("output/output.mp3", "wb") as file:
+    with open("output.mp3", "wb") as file:
         file.write(response.read())
 
 @textToSpeech_bp.route("/solutions", methods=["POST"])
-def get_solutions(question):
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Du bist ein Porsche-Experte und beantwortest Fragen zu Porsche."},
-            {"role": "user", "content": question},
-        ],
-    )
-    content = response.choices[0].message.content
-    
-    create_voice(content)
+def get_solutions():
+        data = request.json
+        question = data.get('question')
+        print(question)
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Du bist ein Porsche-Experte und beantwortest Fragen zu Porsche."},
+                {"role": "user", "content": question},
+            ],
+        )
+        content = response.choices[0].message.content
 
+        create_voice(content)
+
+        return jsonify(content), 200
 
 
